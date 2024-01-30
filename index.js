0369d525e2740d28f0880523b0f3f9b5718f7ceb
Katie <thorburnkatie@gmail.com>
2024-01-30 19:15:53 +1100

    done

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1*num2;
+console.log(multiply);
 
-// Write your code here
+
+let random = Math.round(Math.random())+1;
+console.log(random);
+
+let num3 = 9;
+let num4 = 5;
+let mod = num3-num4;
+console.log(mod);
+
+let max = Math.max(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20);
+console.log(max);
